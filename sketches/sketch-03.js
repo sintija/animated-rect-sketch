f4ff9178ebf3c816a997318972fff00032acd7c0
Sintija Linuza <sintija.linuza@gmail.com>
2022-09-07 23:35:15 +0100

    update to a vector function and cleanup

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -3,8 +3,8 @@ const random = require('canvas-sketch-util/random');
 const math = require('canvas-sketch-util/math');
 
 const settings = {
-  dimensions: [ 1080, 1080 ],
-  animate: true
+	dimensions: [ 1080, 1080 ],
+	animate: true
 };
 
 //animatin function if not using the canvas sketch, canvas sketch function is animate:true
@@ -17,13 +17,14 @@ const settings = {
 
 const sketch = ({ context, width, height }) => {
 
-  const agents =[];
+  const agents = []
 
-  for (let i = 0; i <5; i++ ) {
-    const x = random.range(0,width); 
-    const y = random.range(0,height);
-    agents.push(new Agent(x,y));
-  }
+	for (let i = 0; i < 40; i++) {
+		const x = random.range(0, width);
+		const y = random.range(0, height);
+
+		agents.push(new Agent(x, y));
+	}
 
 
   return ({ context, width, height }) => {
@@ -31,41 +32,34 @@ const sketch = ({ context, width, height }) => {
     context.fillRect(0, 0, width, height);
 
 
-    for(let i = 0; i< agents.length; i++) {
-      //storing agent as a variable
-      const agent = agents[i];
-      //when i=0 j will be one 
+    for (let i = 0; i < agents.length; i++) {
+			const agent = agents[i];
 
-      for(let j = i +1; j < agents.length; j++) {
-        //For each agent we loop through every other agent 
-        const other = agents[j];
+			for (let j = i + 1; j < agents.length; j++) {
+				const other = agents[j];
 
-        const dist = agent.pos.getDistance(other.pos);
+				const dist = agent.pos.getDistance(other.pos);
 
-        if(dist > 200) continue;
+				if (dist > 200) continue;
 
-        context.lineWidth  = math.mapRange(dist, 0,200, 12,1);
+				context.lineWidth = math.mapRange(dist, 0, 200, 12, 1);
 
-        context.beginPath(); 
-        context.moveTo(agent.pos.x, agent.pos.y);
-        context.lineTo(other.pos.x ,other.pos.y);
-        context.stroke();
-
-
-
-      }
-
-    }
+				context.beginPath();
+				context.moveTo(agent.pos.x, agent.pos.y);
+				context.lineTo(other.pos.x, other.pos.y);
+				context.stroke();
+			}
+		}
 
 
     // const point = {x: 800, y: 400, radius: 10};
     // const agentA  = new Agent(800, 400);
     // const agentB  = new Agent(300, 700);
     agents.forEach(agent => {
-      agent.update();
-      agent.draw(context);
-      agent.bounce(width,height)
-    });
+			agent.update();
+			agent.draw(context);
+			agent.bounce(width, height);
+		});
 
   };
 };
@@ -82,7 +76,7 @@ class Vector{
   getDistance(v) {
     const dx = this.x - v.x;
     const dy = this.y - v.y;
-    return Math.sqrt(dx *dy + dy * dy)
+    return Math.sqrt(dx * dx + dy * dy);
 
   }
 }
@@ -93,15 +87,15 @@ class Agent {
   constructor(x,y) {
     this.pos =new Vector(x,y); 
     //Adding agent animation 
-    this.vel = new Vector(random.range(-1,1), random.range(-1,1)); 
-    this.radius = random.range(4,12);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+		this.radius = random.range(4, 12);
   }
 
   //Method for the circles to bounce back once the reached the boundries of canvas
   //invert the velocity on these conditions 
 bounce(width, height) {
-  if(this.pos.x <=0 || this.pos.x >= width) this.vel.x = -1; 
-  if(this.pos.y <=0 || this.pos.y >= width) this.vel.y = -1; 
+  if (this.pos.x <= 0 || this.pos.x >= width)  this.vel.x *= -1;
+  if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
 }
 
 
