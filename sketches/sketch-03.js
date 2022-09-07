56e2f1173e7ad3579dca9dae123b1631fe4ade79
Sintija Linuza <sintija.linuza@gmail.com>
2022-09-07 21:53:56 +0100

    drawing 2 pints on the canvas using classes and objects

---
+++ sketches/sketch-03.js
@@ -0,0 +1,42 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+
+    // const point = {x: 800, y: 400, radius: 10};
+
+    const pointA  = new Point(800, 400,10);
+    const pointB  = new Point(300, 700,10);
+
+
+
+    context.beginPath();
+    context.arc(pointA.x, pointA.y, pointA.radius, 0 , Math.PI *2);
+    context.fillStyle ='black';
+    context.fill();
+
+    context.beginPath();
+    context.arc(pointB.x, pointB.y, pointB.radius, 0 , Math.PI *2);
+    context.fillStyle ='black';
+    context.fill();
+  };
+};
+
+canvasSketch(sketch, settings);
+
+class Point {
+  constructor(x,y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    
+  }
+}
+ 
