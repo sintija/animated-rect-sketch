2c886060b88951bc010924e2602eae34b530e376
Sintija Linuza <sintija.linuza@gmail.com>
2022-09-11 21:56:01 +0100

    tweakpane settings completed

--- sketches/sketch-04.js
+++ sketches/sketch-04.js
@@ -13,6 +13,11 @@ const params = {
   rows: 10, 
   scaleMin: 1, 
   scaleMax: 30,
+  freq: 0.001, 
+  amp: 0.2,
+  frame: 0, 
+  animate: true,
+  lineCap : 'butt',
 
 }; 
 
@@ -54,15 +59,23 @@ const sketch = () => {
       const w = cellw * 0.8;
       const h = cellh * 0.8;
 
+      const f = params.animate ? frame : params.frame; 
+
       //generate the noise 
-      const n = random.noise2D(x + frame * 10, y, 0.001);
+      //const n = random.noise2D(x + frame * 10, y, params.freq);
+
+      const n = random.noise3D(x, y, f * 10, params.freq);
+
+
+
+
       //Set the angle of the rotation 
-      const angle = n * Math.PI * 0.2;
+      const angle = n * Math.PI * params.amp;
 
 
       //const scale = (n + 1) / 2 * 30; 
       //const scale = (n * 0.5 + 0.5) * 30;
-      const scale = math.mapRange(n, -1, 1, 1, 30);
+      const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
 
 
 
@@ -76,6 +89,7 @@ const sketch = () => {
 
 
       context.lineWidth = scale;
+      context.lineCap = params.lineCap; 
 
 
       context.beginPath();
@@ -92,8 +106,19 @@ const createPane = () => {
   const pane = new Tweakpane.Pane();
   let folder; 
   folder = pane.addFolder({title: 'Grid'});
+  folder.addInput(params, 'lineCap', {options: {butt: 'butt', round: 'round', square: 'square'}});
   folder.addInput(params, 'cols', {min: 2, max: 50, step: 1}); 
-  folder.addInput(params, 'rows', {min: 2, max: 50, step: 1}); 
+  folder.addInput(params, 'rows', {min: 2, max: 50, step: 1});
+  folder.addInput(params, 'scaleMin', {min: 1, max: 100});  
+  folder.addInput(params, 'scaleMax', {min: 1, max: 100}); 
+
+
+  folder = pane.addFolder({title: 'Noise'});
+  folder.addInput(params, 'freq', {min: -0.01, max: 0.01});
+  folder.addInput(params, 'amp', {min: 0, max: 1});
+  folder.addInput(params, 'animate');
+  folder.addInput(params, 'frame', {min: 0, max: 999});
+
 };
 
 createPane();
